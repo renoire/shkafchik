bd91eb0ccd52f3398ce229fa898f4b379230fd35
renoire <kronshtein78rus@gmail.com>
2022-05-18 16:50:08 +0300

    logger initialized

--- cmd/main.go
+++ cmd/main.go
@@ -7,11 +7,12 @@ import (
 	"os/signal"
 	"syscall"
 
-	log "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 )
 
 type Config struct {
 	HTTPAddress string
+	LogFile     string
 }
 
 func getCfg() Config {
@@ -20,9 +21,25 @@ func getCfg() Config {
 		httpAddr = "127.0.0.1:8080"
 	}
 
+	logFile := os.Getenv("LOG_FILE")
+	if logFile == "" {
+		logFile = "log.txt"
+	}
+
 	return Config{
 		HTTPAddress: httpAddr,
+		LogFile:     logFile,
 	}
+}
+
+func initLogger(logFile string) {
+	lf, err := os.OpenFile(logFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0755)
+	if err != nil {
+		logrus.Panic("Could not open or create log file: ", err)
+	}
+
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	logrus.SetOutput(lf)
 }
 
 func main() {
@@ -31,6 +48,8 @@ func main() {
 	srv := http.Server{
 		Addr: cfg.HTTPAddress,
 	}
+
+	initLogger(cfg.LogFile)
 
 	sigC := make(chan os.Signal, 1)
 	defer close(sigC)
@@ -42,7 +61,7 @@ func main() {
 	signal.Notify(sigC, syscall.SIGINT, syscall.SIGTERM)
 
 	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		log.Error("error: http server failed: ", err)
+		logrus.Error("error: http server failed: ", err)
 	}
 
 }
