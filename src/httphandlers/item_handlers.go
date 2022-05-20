795180a127185bd18cffeb501646925015dc3f91
renoire <kronshtein78rus@gmail.com>
2022-05-20 18:56:13 +0300

    small fix

--- src/httphandlers/item_handlers.go
+++ src/httphandlers/item_handlers.go
@@ -2,7 +2,7 @@ package httphandlers
 
 import "net/http"
 
-func (*Handlers) GetAllItems(w http.ResponseWriter, r *http.Request) {
+func (*HTTPHandlers) GetAllItems(w http.ResponseWriter, r *http.Request) {
 	type Response struct {
 	}
 	type Request struct {
@@ -11,7 +11,7 @@ func (*Handlers) GetAllItems(w http.ResponseWriter, r *http.Request) {
 	//	writeJSONResponse(w, &res)
 }
 
-func (*Handlers) AddItem(w http.ResponseWriter, r *http.Request) {
+func (*HTTPHandlers) AddItem(w http.ResponseWriter, r *http.Request) {
 	type Response struct {
 	}
 	type Request struct {
@@ -20,7 +20,7 @@ func (*Handlers) AddItem(w http.ResponseWriter, r *http.Request) {
 	//	writeJSONResponse(w, &res)
 }
 
-func (*Handlers) GetItem(w http.ResponseWriter, r *http.Request) {
+func (*HTTPHandlers) GetItem(w http.ResponseWriter, r *http.Request) {
 	type Response struct {
 	}
 	type Request struct {
